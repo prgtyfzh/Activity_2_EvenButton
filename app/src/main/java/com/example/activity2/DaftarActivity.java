2bef1e7c8237938074d44a4241d4327700898221
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-03-15 00:04:16 +0700

    Activity membuat aplikasi menggunakan menu dan floating button

---
+++ app/src/main/java/com/example/activity2/DaftarActivity.java
@@ -0,0 +1,61 @@
+package com.example.activity2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class DaftarActivity extends AppCompatActivity {
+
+    EditText edtNama, edtAlamat, edtEmail, edtPassword, edtrepass;
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar);
+
+        edtNama = findViewById(R.id.edNama);
+        edtAlamat = findViewById(R.id.edAlamat);
+        edtEmail = findViewById(R.id.EdEmail);
+        edtPassword = findViewById(R.id.edPass);
+        edtrepass = findViewById(R.id.edrepas);
+
+        fab = findViewById(R.id.fabSimpan);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtNama.getText().toString().isEmpty() ||
+                edtAlamat.getText().toString().isEmpty() ||
+                edtEmail.getText().toString().isEmpty() ||
+                edtPassword.getText().toString().isEmpty() ||
+                edtrepass.getText().toString().isEmpty())
+                {
+                    Snackbar.make(view,"Wajib isi seluruh data!", Snackbar.LENGTH_LONG).show();
+                }
+                else
+                {
+                    if (edtPassword.getText().toString().equals(edtrepass.getText().toString()))
+                    {
+                        Toast.makeText(DaftarActivity.this, "Pendaftaran Berhasil", Toast.LENGTH_SHORT).show();
+
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                    else
+                    {
+                        Snackbar.make(view, "Password dan Repassword harus sama!", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+}
